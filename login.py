56f1c916a2000a85d41fb4178ca68a70f1bdaba2
Jagrutivyas <52876641+Jagrutivyas@users.noreply.github.com>
2021-08-13 21:20:51 +0530

    Added username password label

--- login.py
+++ login.py
@@ -5,11 +5,22 @@ wnd = Tk()
 
 #name to be changed with newer version release
 wnd.title("PasswordVault 1.0")
+wnd.geometry('500x400')
 
-B = Button(wnd, text ="Sign in", command = Handlers.SignInCallBack)
+#Username Entry
+L1 = Label(wnd, text="UserName").grid(row=1, column=0)
+E1 = Entry(wnd, bd =5).grid(row=1, column=11)
 
-B.pack()
+
+#Password Entry
+L2 = Label(wnd, text="Password").grid(row=9, column=0)
+E2 = Entry(wnd, bd =5,show='*').grid(row=9, column=11)
+
+#Button
+B = Button(wnd, text ="Sign in", command = Handlers.SignInCallBack).grid(row=14, column=0)
+
 # Code to add widgets will go here...
+
 wnd.mainloop()
 
 
